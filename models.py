9fce62e5841e470eee8a54bbcf9ee13224198b48
ljyanesm <yanes.luis@gmail.com>
2021-03-21 15:17:08 +0000

    one-to-one model example

--- models.py
+++ models.py
@@ -24,6 +24,7 @@ class UnitType(db.Model):
 class Unit(db.Model):
     __tablename__ = "unit"
     id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(512), nullable=False)
     unit_type_id = relationship('UnitType', uselist=False, back_populates="unit_type")
 
 
