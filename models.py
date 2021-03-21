f25c5f340ae2abeb2cc7c9107aaa73577e374990
ljyanesm <yanes.luis@gmail.com>
2021-03-21 17:41:58 +0000

    Updated database design
    Added models for Chamber -> Sensor -> Unit

--- models.py
+++ models.py
@@ -6,26 +6,38 @@ from config import db
 from config import ma
 
 
+class Chamber(db.Model):
+    __tablename__ = "chamber"
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(512), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.id'))
+    sensor = relationship("Sensor")
+
+
 class Sensor(db.Model):
     __tablename__ = "sensor"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(512), nullable=False)
-    timestamp = db.Column(
-        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
-    )
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    sensor_unit_id = db.Column(db.Integer, db.ForeignKey('sensor_unit.id'))
+    sensor_unit = relationship("SensorUnit")
 
 
-class UnitType(db.Model):
-    __tablename__ = "unit_type"
+class SensorUnit(db.Model):
+    __tablename__ = "sensor_unit"
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(512), nullable=False)
+    min = db.Column(db.Float)
+    max = db.Column(db.Float)
+    unit_id = db.Column(db.Integer, db.ForeignKey('unit.id'))
+    unit = relationship('Unit', back_populates='sensor_unit')
 
 
 class Unit(db.Model):
     __tablename__ = "unit"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(512), nullable=False)
-    unit_type_id = relationship('UnitType', uselist=False, back_populates="unit_type")
+    sensor_unit = relationship('SensorUnit', uselist=False, back_populates='unit')
 
 
 class SensorSchema(ma.SQLAlchemyAutoSchema):
