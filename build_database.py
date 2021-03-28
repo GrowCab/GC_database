618e7e40464b612602e838b30b3b15302d251fc9
ljyanesm <yanes.luis@gmail.com>
2021-03-28 22:52:54 +0100

    Add a configuration to the example db chamber

--- build_database.py
+++ build_database.py
@@ -1,6 +1,8 @@
 import os
+from datetime import datetime
+
 from config.config import db
-from models.models import Sensor, Chamber, Unit, SensorUnit
+from models.models import Sensor, Chamber, Unit, SensorUnit, Configuration, ExpectedMeasure
 
 if __name__ == "__main__":
 
@@ -14,8 +16,8 @@ if __name__ == "__main__":
     # populate the database
 
     # Add a new example chamber
-
-    db.session.add(Chamber(description="Example"))
+    chamber1 = Chamber(description="Example")
+    db.session.add(chamber1)
     db.session.commit()
 
     # Add units
@@ -24,25 +26,41 @@ if __name__ == "__main__":
     hum = Unit(description='hum')
     cel = Unit(description='C')
 
-    db.session.bulk_save_objects([lux, hum, cel])
+    db.session.bulk_save_objects([lux, hum, cel], return_defaults=True)
     db.session.commit()
 
     # Add a new sensor
 
-    temperature_and_humidity_sensor = Sensor(description="BME280", chamber_id=1)
-    illumination_sensor = Sensor(description="ILUM", chamber_id=1)
-    db.session.bulk_save_objects([temperature_and_humidity_sensor, illumination_sensor])
+    temperature_and_humidity_sensor = Sensor(description="BME280", chamber_id=chamber1.id)
+    illumination_sensor = Sensor(description="ILUM", chamber_id=chamber1.id)
+    db.session.bulk_save_objects([temperature_and_humidity_sensor, illumination_sensor], return_defaults=True)
 
     # Add sensor units
 
-    celsius_unit = SensorUnit(min=-30, max=90, unit_id=3, sensor_id=1)
-    humidity_unit = SensorUnit(min=0, max=100, unit_id=2, sensor_id=1)
-    lux_unit = SensorUnit(min=0, max=10000, unit_id=1, sensor_id=2)
-    db.session.bulk_save_objects([celsius_unit, humidity_unit, lux_unit])
+    celsius_unit = SensorUnit(min=-30, max=90, unit_id=cel.id, sensor_id=temperature_and_humidity_sensor.id)
+    humidity_unit = SensorUnit(min=0, max=100, unit_id=hum.id, sensor_id=temperature_and_humidity_sensor.id)
+    lux_unit = SensorUnit(min=0, max=10000, unit_id=lux.id, sensor_id=illumination_sensor.id)
+    db.session.bulk_save_objects([celsius_unit, humidity_unit, lux_unit], return_defaults=True)
+    db.session.commit()
+
+    # Add a configuration to the chamber
+
+    chamber1_configuration = Configuration(chamber=chamber1, start=datetime.now(), description="Until forever")
+    db.session.add(chamber1_configuration)
+    db.session.commit()
+
+    expected1 = ExpectedMeasure(expected_value=28, start_hour=0, start_minute=0, end_hour=6, end_minute=0,
+                                configuration=chamber1_configuration, unit=cel)
+    expected2 = ExpectedMeasure(expected_value=16, start_hour=6, start_minute=1, end_hour=23, end_minute=59,
+                                configuration=chamber1_configuration, unit=cel)
+
+    db.session.add(expected1)
+    db.session.add(expected2)
     db.session.commit()
 
     all_sensors = Sensor.query.all()
     all_chambers = Chamber.query.all()
     all_sensor_units = SensorUnit.query.all()
     all_units = Unit.query.all()
+    all_configurations = Configuration.query.all()
     [print(s) for s in all_sensors]
