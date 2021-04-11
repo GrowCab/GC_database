f6ce825817d4419685ba441c391980ecf328da30
ljyanesm <yanes.luis@gmail.com>
2021-04-11 11:56:12 +0100

    Get ExpectedMeasures for each of the units in a chamber

--- api/chamber_schedule.py
+++ api/chamber_schedule.py
@@ -3,7 +3,7 @@ from time import sleep
 from flask.views import MethodView
 from flask_smorest import Blueprint
 
-from models.models import ConfigurationSchema, Configuration
+from models.models import ConfigurationSchema, Configuration, ExpectedMeasure, ExpectedMeasureSchema
 
 schedule_blp = Blueprint('chamber_schedule', 'chamber_schedule',
                          url_prefix='/api',
@@ -19,3 +19,12 @@ class ChamberSchedule(MethodView):
         configuration = Configuration.query.filter(Configuration.chamber_id == chamber_id).\
             order_by(Configuration.timestamp).one_or_none()
         return configuration
+
+
+@schedule_blp.route('/chamber_schedule_unit/<int:chamber_id>/<int:unit_id>')
+class ChamberScheduleUnit(MethodView):
+    @schedule_blp.doc(operationId='getChamberScheduleUnit')
+    @schedule_blp.response(200, ExpectedMeasureSchema(many=True))
+    def get(self, chamber_id: int, unit_id: int):
+        return ExpectedMeasure.query.join(Configuration).filter(Configuration.chamber_id == chamber_id).\
+            filter(ExpectedMeasure.unit_id == unit_id).all()
