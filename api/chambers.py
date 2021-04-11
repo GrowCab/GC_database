dd4eef9191f6a4561c6747bae2574bfb01d9f8a8
ljyanesm <yanes.luis@gmail.com>
2021-04-11 01:47:02 +0100

    Add a chambers REST API

---
+++ api/chambers.py
@@ -0,0 +1,49 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint
+
+from models.models import Chamber, ChamberSensor, ChamberSchema, Unit, SensorUnit, Sensor, UnitSchema, Configuration
+
+chamber_blp = Blueprint('chambers', 'chambers',
+                        url_prefix='/api',
+                        description='Operations on Chambers')
+
+
+@chamber_blp.route('/chambers')
+class ChambersList(MethodView):
+    @chamber_blp.doc(operationId='getChambers')
+    @chamber_blp.response(200, ChamberSchema(many=True))
+    def get(self):
+        """Get the list of configurations
+
+        -------------------
+        :return: Returns a list of configurations objects
+        """
+        return Chamber.query.all()
+
+
+@chamber_blp.route('/chamber/<int:chamber_id>')
+class ChamberAPI(MethodView):
+    @chamber_blp.doc(operationId='getChamber')
+    @chamber_blp.response(200, ChamberSchema)
+    def get(self, chamber_id):
+        """Get the
+        :param chamber_id: ID of the chamber
+        :return: Returns a Chamber object
+        """
+        return Chamber.query.filter(Chamber.id == chamber_id).one_or_none()
+
+
+@chamber_blp.route('/chamber_units/<int:chamber_id>')
+class ChamberUnitsAPI(MethodView):
+    @chamber_blp.doc(operationId='getChamberUnits')
+    @chamber_blp.response(200, UnitSchema(many=True))
+    def get(self, chamber_id):
+        """Get the units available for this chamber
+
+        This is useful for understanding which dials to present but also which values to use for filtering/separating
+        the ExpectedMeasure(s) of a Configuration for a Chamber
+        :param chamber_id: ID of the chamber
+        :return: Returns a list of Unit objects
+        """
+        return Unit.query.join(SensorUnit.sensor).join(Sensor.chamber_sensor).\
+            filter(ChamberSensor.chamber_id == chamber_id).all()
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from api.chamber_schedule import schedule_blp
+from api.chambers import chamber_blp
 from api.configurations import configuration_blp
 from api.sensors import sensors_blp, sensor_blp
 from config import config
@@ -9,5 +10,6 @@ api.register_blueprint(sensors_blp)
 api.register_blueprint(configuration_blp)
 api.register_blueprint(sensor_blp)
 api.register_blueprint(schedule_blp)
+api.register_blueprint(chamber_blp)
 if __name__ == '__main__':
     app.run(debug=True)
