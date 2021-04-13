16243ee311cca5253a3974956c6c573bef4d5bd7
ljyanesm <yanes.luis@gmail.com>
2021-04-13 22:04:08 +0100

    Insert Configuration API calls
    - Leave the API call for inserting configurations in the same section as for getting them
    - Remove the extra configuration insertion method in chamber_schedule.py
    - Add an example of a configuration to insert to the docs of the Configuration put call
    - Make sure only the latest configuration is returned when requesting the chamber schedule

--- api/chamber_schedule.py
+++ api/chamber_schedule.py
@@ -1,11 +1,8 @@
 from time import sleep
 
-from flask import abort
 from flask.views import MethodView
 from flask_smorest import Blueprint
 
-from config.config import db
-from .chambers import get_chamber_units
 from models.models import ConfigurationSchema, Configuration, ExpectedMeasure, ExpectedMeasureSchema
 
 schedule_blp = Blueprint('chamber_schedule', 'chamber_schedule',
@@ -20,48 +17,8 @@ class ChamberSchedule(MethodView):
     def get(self, chamber_id: int):
         sleep(1)
         configuration = Configuration.query.filter(Configuration.chamber_id == chamber_id).\
-            order_by(Configuration.timestamp).one_or_none()
+            order_by(Configuration.timestamp.desc()).first()
         return configuration
-
-    @schedule_blp.doc(operationId='putChamberSchedule')
-    @schedule_blp.arguments(ConfigurationSchema)
-    @schedule_blp.response(200, ConfigurationSchema)
-    def put(self, configuration: Configuration):
-        chamber_units = get_chamber_units(configuration.chamber_id)
-        is_valid = True
-
-        # Check if:
-        for unit in chamber_units:
-            unit_interval = [em for em in configuration.expected_measure if em.unit_id == unit.id]
-            #   - There is a range for each unit in the chamber
-            if not unit_interval:
-                is_valid = False
-                break
-            #   - Each units' configuration is a valid 24h interval
-            for interval in unit_interval:
-                #   - Each interval is within 0000 - 2359
-                if 0 < interval.end_minute > 59:
-                    is_valid = False
-                    break
-                if 0 < interval.end_hour > 23:
-                    is_valid = False
-                    break
-            # - The last interval ends at 2359
-            if unit_interval[-1].end_minute != 59:
-                is_valid = False
-            if unit_interval[-1].end_hour != 23:
-                is_valid = False
-
-            if not is_valid:
-                break
-
-        if is_valid:
-            db.session.add(configuration)
-            return configuration
-        else:
-            return abort(409,
-                         f"The configuration provided {configuration} is not valid",
-                         )
 
 
 @schedule_blp.route('/chamber_schedule_unit/<int:chamber_id>/<int:unit_id>')
--- api/configurations.py
+++ api/configurations.py
@@ -5,6 +5,7 @@ from flask import abort
 from flask.views import MethodView
 from flask_smorest import Blueprint
 
+from api.chambers import get_chamber_units
 from config.config import db
 from models.models import Configuration, ConfigurationSchema
 
@@ -32,21 +33,68 @@ class ConfigurationsList(MethodView):
         """Stores a new configuration
 
         ----------------------------
+
+        This is an example of the data required to insert a new configuration
+        {
+          "description": "Initial configuration",
+          "expected_measure": [
+            {
+              "expected_value": 10,
+              "end_hour": 23,
+              "end_minute": 59,
+              "unit_id": 1
+            },
+            {
+              "expected_value": 10,
+              "end_hour": 23,
+              "end_minute": 59,
+              "unit_id": 2
+            },
+            {
+              "expected_value": 10,
+              "end_hour": 23,
+              "end_minute": 59,
+              "unit_id": 3
+            }
+          ],
+          "chamber_id": 1
+        }
         :param configuration:
         :return:
         """
-        existing_configuration = Configuration.query.filter(
-            Configuration.description == configuration.description,
-            Configuration.chamber_id == configuration.chamber_id,
-            Configuration.start == configuration.start,
-            Configuration.end == configuration.end,
-            Configuration.expected_measure == configuration.expected_measure).one_or_none()
 
-        if existing_configuration is None:
+        chamber_units = get_chamber_units(configuration.chamber_id)
+        is_valid = True
+
+        # Check if:
+        for unit in chamber_units:
+            unit_interval = [em for em in configuration.expected_measure if em.unit_id == unit.id]
+            #   - There is a range for each unit in the chamber
+            if not unit_interval:
+                is_valid = False
+                break
+            #   - Each units' configuration is a valid 24h interval
+            for interval in unit_interval:
+                #   - Each interval is within 0000 - 2359
+                if 0 < interval.end_minute > 59:
+                    is_valid = False
+                    break
+                if 0 < interval.end_hour > 23:
+                    is_valid = False
+                    break
+            # - The last interval ends at 2359
+            if unit_interval[-1].end_minute != 59:
+                is_valid = False
+            if unit_interval[-1].end_hour != 23:
+                is_valid = False
+
+            if not is_valid:
+                break
+        if is_valid:
             db.session.add(configuration)
             db.session.commit()
 
             return configuration, 201
         else:
-            abort(409,
-                  f"Configuration {configuration} exists already")
+            abort(400,
+                  f"Configuration {configuration} is invalid")
