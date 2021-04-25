dbf0ff2e3447ca0d71b95d62702c8f48779ee829
ljyanesm <yanes.luis@gmail.com>
2021-04-25 03:31:59 +0100

    Update main so it accepts external connections

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ api.register_blueprint(sensor_blp)
 api.register_blueprint(schedule_blp)
 api.register_blueprint(chamber_blp)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
